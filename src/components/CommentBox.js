45a3c547baa33e71d85fd287cab9493415bde584
NoriX <versatility.95@mail.ru>
2019-03-31 19:45:43 +0300

    Step 3: Placing Reusable logic into HOC

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,21 +5,6 @@ import * as actions from '../actions'
 class CommentBox extends React.Component {
     state = { comment: '' }
 
-    componentDidMount() {
-        this.shouldNavigateAway();
-    }
-
-    componentDidUpdate() {
-        this.shouldNavigateAway();
-    }
-
-    shouldNavigateAway() {
-        if (!this.props.auth) {
-            console.log('I NEED TO LEAVE!!');
-            this.props.history.push('/');
-        }
-    }
-
     handleChange = (event) => {
         this.setState({ comment: event.target.value });
     };
@@ -42,8 +27,4 @@ class CommentBox extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return { auth: state.auth }
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(CommentBox);
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 export default (ChildComponent) => {
     class ComposedComponent extends React.Component {
+
+        componentDidMount() {
+            this.shouldNavigateAway();
+        }
+
+        componentDidUpdate() {
+            this.shouldNavigateAway();
+        }
+
+        shouldNavigateAway() {
+            if (!this.props.auth) {
+                console.log('I NEED TO LEAVE!!');
+                this.props.history.push('/');
+            }
+        }
+
         render() {
             return <ChildComponent />
         }
     }
 
-    return ComposedComponent;
+    const mapStateToProps = (state) => {
+        return { auth: state.auth }
+    }
+
+    return connect(mapStateToProps)(ComposedComponent);
 };
 
 //Higher Order Component - HOC
