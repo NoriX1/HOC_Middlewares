365e514c108032550ff04e09df1f2c934fa6351d
NoriX <versatility.95@mail.ru>
2019-03-31 18:56:48 +0300

    Reducers, Actions, Redux-Thunk middleware

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import commentReducer from './commentReducer';
+import authReducer from './authReducer';
+
+export default combineReducers({
+    comments: commentReducer,
+    auth: authReducer
+});
