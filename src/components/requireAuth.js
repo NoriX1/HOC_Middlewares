c483e6aed3c16ab7b2088d1810f2973eff27a663
NoriX <versatility.95@mail.ru>
2019-03-31 19:41:56 +0300

    Step 2: Boilerplate code (~template) of Higher Order Component

---
+++ src/components/requireAuth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (ChildComponent) => {
+    class ComposedComponent extends React.Component {
+        render() {
+            return <ChildComponent />
+        }
+    }
+
+    return ComposedComponent;
+};
+
+//Higher Order Component - HOC
