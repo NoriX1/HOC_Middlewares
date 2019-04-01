3e5ce65d062a7fdfbc89865b414a67f63a2a2211
NoriX <versatility.95@mail.ru>
2019-04-01 17:17:12 +0300

    stateValidator custom middleware

---
+++ src/middlewares/stateValidator.js
@@ -0,0 +1,11 @@
+import stateSchema from './stateSchema';
+import tv4 from 'tv4';
+
+export default ({ dispatch, getState }) => (next) => (action) => {
+    next(action);
+    // This code will be runned after the action has gone through all of the Redux Store;
+
+    if (!tv4.validate(getState(), stateSchema)) {
+        console.warn('Invalid state schema detected');
+    }
+};
